d998ab9988601f9964091b88ad13f3e42c601f1d
Todd Ryan Whitten <todd.ryan.whitten@gmail.com>
2026-04-08 21:45:54 +0000

    wip

--- upload.py
+++ upload.py
@@ -2,19 +2,23 @@ from google.cloud import storage
 from google.oauth2 import service_account
 
 CREDENTIALS_FILE = "keys/mnist.json"
-BUCKET_NAME = "whitten-data-bucket"
-SOURCE_FILE = "model/mnist_model.h5"
-DESTINATION_BLOB = "models/mnist/mnist_model.h5"
-
+DESTINATION_BUCKET_NAME = "whitten-data-bucket"
 
 def upload_model():
+
+    SOURCE_FILE = "model/mnist_model.h5"
+    DESTINATION_FILEPATH = "models/mnist/mnist_model.h5"
+
     credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
     client = storage.Client(credentials=credentials)
-    bucket = client.bucket(BUCKET_NAME)
-    blob = bucket.blob(DESTINATION_BLOB)
+    bucket = client.bucket(DESTINATION_BUCKET_NAME)
+    blob = bucket.blob(DESTINATION_FILEPATH)
 
     blob.upload_from_filename(SOURCE_FILE)
-    print(f"Uploaded {SOURCE_FILE} to gs://{BUCKET_NAME}/{DESTINATION_BLOB}")
+    print(f"Uploaded {SOURCE_FILE} to gs://{DESTINATION_BUCKET_NAME}/{DESTINATION_FILEPATH}")
+
+# def upload_js():
+
 
 
 if __name__ == "__main__":
