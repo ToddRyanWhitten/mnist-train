6785c2d5f78d1e8a40fe5f143ae75b288d2c5852
Todd Ryan Whitten <todd.ryan.whitten@gmail.com>
2026-04-08 22:24:52 +0000

    wip

--- upload.py
+++ upload.py
@@ -1,3 +1,4 @@
+import os
 from google.cloud import storage
 from google.oauth2 import service_account
 
@@ -6,8 +7,8 @@ DESTINATION_BUCKET_NAME = "whitten-data-bucket"
 
 def upload_model():
 
-    SOURCE_FILE = "model/mnist_model.h5"
-    DESTINATION_FILEPATH = "models/mnist/mnist_model.h5"
+    SOURCE_FILE = "models/mnist/v0.0.1/mnist.h5"
+    DESTINATION_FILEPATH = "models/mnist/v0.0.1/mnist.h5"
 
     credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
     client = storage.Client(credentials=credentials)
@@ -17,9 +18,24 @@ def upload_model():
     blob.upload_from_filename(SOURCE_FILE)
     print(f"Uploaded {SOURCE_FILE} to gs://{DESTINATION_BUCKET_NAME}/{DESTINATION_FILEPATH}")
 
-# def upload_js():
+def upload_js():
 
+    SOURCE_DIR = "models/mnist"
+    DESTINATION_PREFIX = "models/mnist"
 
+    credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
+    client = storage.Client(credentials=credentials)
+    bucket = client.bucket(DESTINATION_BUCKET_NAME)
+
+    for root, _, files in os.walk(SOURCE_DIR):
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            relative_path = os.path.relpath(local_path, SOURCE_DIR)
+            destination_path = f"{DESTINATION_PREFIX}/{relative_path}"
+            blob = bucket.blob(destination_path)
+            blob.upload_from_filename(local_path)
+            print(f"Uploaded {local_path} to gs://{DESTINATION_BUCKET_NAME}/{destination_path}")
 
 if __name__ == "__main__":
     upload_model()
+    upload_js()
